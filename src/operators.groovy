db8d67527eba1bff92c716b078395273ae6547bd
Sidharth Tiu <tiu.avinash05@gmail.com>
2016-06-09 12:39:39 +0530

    Some operations using groovy syntax

--- src/operators.groovy
+++ src/operators.groovy
@@ -1,23 +1,10 @@
 List a = [2, 43, 4, 76, 7, 40, 50, 32, 34]
 List b = []
 
-int j=0
-//Printing element of List a
-a.each { item ->
-    if(item > 20) {
-        println(item)
-    }
+println a.find {
+    return it > 20
 }
 
-//Assigning some value of List a to List b
-a.each { item ->
-    if(item > 20) {
-        b[j]=item
-        j++
-    }
-}
-
-//printing List b
-b.each { hello ->
-    println hello
+println a.findAll {
+    return it > 20
 }
